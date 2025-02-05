e3a7577a3e12ca907dac5fd275469c9d530e2e3a
Varshni D S <127740252+Varshnids@users.noreply.github.com>
2025-02-05 13:29:29 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/ar
-    docker push ar8888/ar
+    docker login -u varshni057 -p @Varshni@1407@
+    docker tag test varshni057/sample
+    docker push varshni057/sample
     
